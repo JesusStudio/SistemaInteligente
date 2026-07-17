b2e6504d5ce0319a610cb49b9545adf4aa61311a
JesusStudio <220844171+JesusStudio@users.noreply.github.com>
2026-07-17 02:14:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import time
 
 import numpy as np
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 from sklearn.compose import ColumnTransformer
